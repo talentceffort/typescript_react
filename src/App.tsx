da11460ea43842c36815e7c193ba059efa415617
talentceffort <talentceffort@gmail.com>
2020-04-26 22:54:03 +0900

    basic typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Greetings from './Greetings';
+import Counter from './Counter'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const onClick = (name: string) => {
+    //   console.log(`${name} says hello`);
+    // };
+    // return <Greetings name="Hello" onClick={onClick} />;
+    return <Counter />
 }
 
 export default App;
